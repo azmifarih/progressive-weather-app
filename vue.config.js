5c501952d3e6a2cfa058986119357e4890af1740
Muhammad Azmi Farih <muazfa@gmail.com>
2021-12-31 23:04:13 +0000

    Fix to combine js and css

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-    baseUrl: '/progressive-weather-app/',
+    publicPath: '/progressive-weather-app/',
     pwa: {
         themeColor: '#6CB9C8',
         msTileColor: '#484F60'
+    },
+    css: {
+	extract: false,
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        }
     }
 }
